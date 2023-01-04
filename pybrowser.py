e7166e88aacd587c090e580137fd057a3fad6ba2
jedwilshire <jedwilshire@gmail.com>
2023-01-04 15:57:13 -0600

    Completed Example for Simple Web Browser

--- pybrowser.py
+++ pybrowser.py
@@ -3,22 +3,24 @@ from settings import *
 from PyQt5.QtWidgets import QMainWindow, QApplication, QToolBar, QPushButton, QLineEdit
 from PyQt5.QtGui import QIcon, QFont
 from PyQt5.QtCore import QSize, QUrl
-from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEnginePage
+from PyQt5.QtWebEngineWidgets import QWebEngineView
 
 # global variables
-app = QApplication([])
+app = QApplication(sys.argv)
 app.webEngineView = QWebEngineView()
 app.addressBar = QLineEdit()
 
-
-def updateAddressBar():
-    app.addressBar.setText(app.webEngineView.url().toString())
+def updateAddressBar(address):
+    app.addressBar.setText(address.toString())
 
 def back():
     app.webEngineView.back()
 
 def forward():
     app.webEngineView.forward()
+
+def stop():
+    app.webEngineView.stop()
 
 def search():
     request = app.addressBar.text()
@@ -36,43 +38,44 @@ def home():
 
 def main():
     window = QMainWindow()
+#     window.setStyleSheet(STYLE_SHEET)
     window.show()
     window.setWindowTitle("Py Browser")
     window.setWindowIcon(QIcon("icons/planet.png"))
-    window.setGeometry(100, 50, WIDTH, HEIGHT)
+    window.setGeometry(50, 50, WIDTH, HEIGHT)
     toolbar = QToolBar()
     window.addToolBar(toolbar)
 
     # adding back button
     backButton = QPushButton()
     backButton.setIcon(QIcon("icons/arrowLeft.png"))
-    backButton.setIconSize(QSize(32, 32))
+    backButton.setIconSize(QSize(ICON_SIZE, ICON_SIZE))
     backButton.clicked.connect(back)  # call the back function when clicked
     toolbar.addWidget(backButton)
 
     # adding forward button
     forwardButton = QPushButton()
     forwardButton.setIcon(QIcon("icons/arrowRight.png"))
-    forwardButton.setIconSize(QSize(32, 32))
+    forwardButton.setIconSize(QSize(ICON_SIZE, ICON_SIZE))
     forwardButton.clicked.connect(forward)  # call the forward function when clicked
     toolbar.addWidget(forwardButton)
 
     # adding reload button
     reloadButton = QPushButton()
     reloadButton.setIcon(QIcon("icons/reload.png"))
-    reloadButton.setIconSize(QSize(32, 32))
+    reloadButton.setIconSize(QSize(ICON_SIZE, ICON_SIZE))
     reloadButton.clicked.connect(reload)  # call the reload function when clicked
     toolbar.addWidget(reloadButton)
 
     # adding home button
     homeButton = QPushButton()
     homeButton.setIcon(QIcon("icons/home.png"))
-    homeButton.setIconSize(QSize(32, 32))
+    homeButton.setIconSize(QSize(ICON_SIZE, ICON_SIZE))
     homeButton.clicked.connect(home)
     toolbar.addWidget(homeButton)
 
     # adding address line
-    app.addressBar.setFont(QFont("TimesNewRoman", 14))
+    app.addressBar.setFont(QFont(FONT, ADDRESS_BAR_FONT_SIZE))
     app.addressBar.setText(HOME)
     app.addressBar.returnPressed.connect(search)  # call search on return key
     toolbar.addWidget(app.addressBar)
@@ -80,7 +83,7 @@ def main():
     # adding search button
     searchButton = QPushButton()
     searchButton.setIcon(QIcon("icons/magnifier.png"))
-    searchButton.setIconSize(QSize(32, 32))
+    searchButton.setIconSize(QSize(ICON_SIZE, ICON_SIZE))
     searchButton.clicked.connect(search)  # call the search function when clicked
     toolbar.addWidget(searchButton)
 
@@ -88,8 +91,8 @@ def main():
     window.setCentralWidget(app.webEngineView)
     app.webEngineView.load(QUrl(app.addressBar.text()))
 
-    # adding event to change addressBar
-    app.webEngineView.loadFinished.connect(updateAddressBar)
+    # adding event to change addressBar text when url address of browswer changed
+    app.webEngineView.urlChanged.connect(updateAddressBar)
     app.exec_()
 
 main()
--- settings.py
+++ settings.py
@@ -1,3 +1,12 @@
-HOME = 'https://www.pygame.org/news'
+HOME = 'https://www.google.com'
 WIDTH = 1240
-HEIGHT = 750
+HEIGHT = 650
+FONT = 'TimesNewRoman'
+ADDRESS_BAR_FONT_SIZE = 10
+ICON_SIZE = 16
+# STYLE_SHEET = """
+# QToolBar {background-color: indigo}
+# QLineEdit {background-color: mediumpurple}
+# QPushButton {background-color: indigo}
+# background-color: indigo
+# """
